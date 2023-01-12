dd7f7e053491eb061caddf59e32f90d3f512747e
achyuta-yadunandan <66506068+achyuta-yadunandan@users.noreply.github.com>
2023-01-12 15:57:50 -0800

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 12 15:55:04 2023
+
+@author: achyu
+"""
+if __name__ == '__main__':
+    x = "ECE_180_DA_DB"
+    if x == "EE_180_DA_DB":
+        print("You are living in 2017")
+    else:
+        # this is a comment
+        x = x + " - Best class ever"
+        print(x)
